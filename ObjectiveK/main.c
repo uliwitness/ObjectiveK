1a25772d652748a4b88061ef72dfb4f84270edbb
Uli Kusterer <ulivc@zathras.de>
2014-10-27 07:30:28 +0100

    Initial Commit

---
+++ ObjectiveK/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ObjectiveK
+//
+//  Created by Uli Kusterer on 2014-10-27.
+//  Copyright (c) 2014 Uli Kusterer. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
